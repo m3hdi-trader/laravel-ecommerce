bfb952dd1e5d640c0b614c639d20aa6474aef250
mehdi <m3hdi.trader@gmail.com>
2024-08-10 11:52:57 +0330

    create-template-admin-attribute implemented

--- resources/views/admin/attributes/index.blade.php
+++ resources/views/admin/attributes/index.blade.php
@@ -11,10 +11,10 @@
     <div class="row">
         <div class="col-xl-12 col-md-12 mb-4 p-md-5 bg-white">
             <div class="d-flex justify-content-between mb-4">
-                <h5 class="font-weight-bold">لیست برند ها ({{ $brands->total() }})</h5>
-                <a class="btn btn-sm btn-outline-primary" href="{{ route('admin.brands.create') }}">
+                <h5 class="font-weight-bold">لیست ویژگی ها ({{ $attributes->total() }})</h5>
+                <a class="btn btn-sm btn-outline-primary" href="{{ route('admin.attributes.create') }}">
                     <i class="fa fa-plus"></i>
-                    ایجاد برند
+                    ایجاد ویژگی
                 </a>
             </div>
             <div>
@@ -23,33 +23,26 @@
                         <tr>
                             <th>#</th>
                             <th>نام</th>
-                            <th>وضعیت</th>
                             <th>عملیات</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($brands as $key => $brand)
+                        @foreach ($attributes as $key => $attribute)
                             <tr>
                                 <th>
-                                    {{ $brands->firstItem() + $key }}
+                                    {{ $attributes->firstItem() + $key }}
                                 </th>
 
                                 <th>
-                                    {{ $brand->name }}
-                                </th>
-
-                                <th>
-                                    <span class="{{ $brand->getRawOriginal('is_active') ? 'text-success' : 'text-danger' }}">
-                                        {{ $brand->is_active }}
-                                    </span>
+                                    {{ $attribute->name }}
                                 </th>
 
                                 <th>
                                     <a class="btn btn-sm btn-outline-success"
-                                        href="{{ route('admin.brands.show', ['brand' => $brand->id]) }}">نمایش</a>
+                                        href="{{ route('admin.attributes.show', ['attribute' => $attribute->id]) }}">نمایش</a>
 
                                     <a class="btn btn-sm btn-outline-info mr-3"
-                                        href="{{ route('admin.brands.edit', ['brand' => $brand->id]) }}">ویرایش</a>
+                                        href="{{ route('admin.attributes.edit', ['attribute' => $attribute->id]) }}">ویرایش</a>
                                 </th>
                             </tr>
                         @endforeach
--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -34,6 +34,19 @@
         </a>
     </li>
 
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProducts"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-cart-plus"></i>
+            <span> محصولات </span>
+        </a>
+        <div id="collapseProducts" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('admin.attributes.index') }}">ویژگی ها</a>
+            </div>
+        </div>
+    </li>
+
 
 
 
